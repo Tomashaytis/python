7d0f81e9418a0de7b75ef0764ac16bdac1b1aacc
pavel <tomashaytis@gmail.com>
2022-09-22 23:16:02 +0400

    Разбирался, как работает commit (думал, что старый commit не сохранился)

--- Lab1/Lab1.py
+++ Lab1/Lab1.py
@@ -1,8 +1,8 @@
-import os
 import requests
 import re
 
 URL = 'https://yandex.ru/images/'
-html_page = requests.get(URL+'search?text=' + 'tiger')
+html_page = requests.get(URL+'search?text=' + 'tiger', headers={"User-Agent": "Mozilla/5.0"})
+# print(html_page.text)
 a = re.findall(r'"origin":\{"w":\w+,"h":\w+,"url":"([^"]+)"\}', html_page.text)
 print(a)
