9324d354436494a199be881f2df4f679362ca538
pavel <tomashaytis@gmail.com>
2022-09-26 19:31:19 +0400

    Начал качать менее размерные изображения, полностью отказался от использования шаблонов, изменил способ сборки URL (через использование f-строк). Поменял значения в файле ".gitignore".

--- Lab1/Lab1.py
+++ Lab1/Lab1.py
@@ -6,26 +6,35 @@ from bs4 import BeautifulSoup
 
 def download_picture(direct, img_id, img_address):
     filename = 'dataset/' + direct + '/' + str(img_id).rjust(4, '0') + '.jpg'
-    image = requests.get('https' + img_address, headers)
+    picture = requests.get('https' + img_address, headers)
     if image.status_code:
         direct = open(filename, 'wb')
-        direct.write(image.content)
+        direct.write(picture.content)
         direct.close()
 
 
 headers = {"User-Agent": "Mozilla/5.0"}
+name = 'tiger'
+URL = 'https://yandex.ru/images/'
 # os.mkdir('dataset')
 # os.mkdir('dataset/tiger')
 # os.mkdir('dataset/leopard')
-URL = 'https://yandex.ru/images/search?text=tiger%20animal%20'
 images = []
+max_pages = 20
+for page in range(0, max_pages):
+    html_page = requests.get(f'{URL}search?p={page}&text={name}&lr=51&rpt=image', headers).text
+    soup = BeautifulSoup(html_page, 'lxml')
+    page_img = soup.find_all('img')
+    for image in page_img:
+        images.append(image['src'])
+print(images)
+print(len(images))
+'''
 max_pages = 40
 for page in range(0, max_pages + 1):
     cur_URL = URL + str(page)
     html_page = requests.get(cur_URL, headers).text
     soup = BeautifulSoup(html_page, 'lxml')
-    print(soup)
-    break
     divs = soup.find_all('div', {"class": re.compile(r"serp-item serp-item_type_search serp-item_group_search serp-item_pos_\d+ serp-item_scale_yes justifier__item i-bem$")})
     for i in range(len(divs)):
         img = re.findall(r'"img_href":"([^"]+)"', str(divs[i]))
@@ -33,7 +42,7 @@ for page in range(0, max_pages + 1):
 print(len(images))
 for i in range(len(images)):
     print(images[i])
-    download_picture('tiger', i, images[i])
+    download_picture(tiger, i, images[i])
 del images
 images = []
 URL = 'https://yandex.ru/images/search?text=leopard%20animal%20'
@@ -47,5 +56,5 @@ for page in range(1, max_pages + 1):
         img = re.findall(r'"origin":\{"w":\w+,"h":\w+,"url":"([^"]+)"\}', str(divs[i]))
         images.append(img[0])
 for i in range(len(images)):
-    download_picture('leopard', i, images[i])
-del images
+    download_picture(leopard, i, images[i])
+del images'''
