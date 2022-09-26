653b93f4ccf9542175b8c7b131e8e605e7237634
pavel <tomashaytis@gmail.com>
2022-09-26 14:10:05 +0400

    Удалил лишние комментарии, продублировал код для второго класса изображений (Код не работает, так как я пытаюсь скачать полноценные изображения, и сайт обрывает соединение)

--- Lab1/Lab1.py
+++ Lab1/Lab1.py
@@ -2,62 +2,50 @@ import os
 import requests
 import re
 from bs4 import BeautifulSoup
-import cv2
-from fake_useragent import UserAgent
 
 
 def download_picture(direct, img_id, img_address):
-    filename = 'dataset/' + direct + '/' + '0' * (4 - len(str(img_id))) + str(img_id) + '.jpg'
-    image = requests.get(img_address)
-    print(image)
-    direct = open(filename, 'wb')
-    direct.write(image.content)
-    direct.close()
+    filename = 'dataset/' + direct + '/' + str(img_id).rjust(4, '0') + '.jpg'
+    image = requests.get('https' + img_address, headers)
+    if image.status_code:
+        direct = open(filename, 'wb')
+        direct.write(image.content)
+        direct.close()
 
 
+headers = {"User-Agent": "Mozilla/5.0"}
 # os.mkdir('dataset')
 # os.mkdir('dataset/tiger')
 # os.mkdir('dataset/leopard')
-URL = 'https://yandex.ru/images/search?text=animal,tiger'
-headers = {"User-Agent": UserAgent().chrome}
-# download_picture('tiger', 0, 'https://www.fonstola.ru/images/201509/fonstola.ru_202497.jpg')
+URL = 'https://yandex.ru/images/search?text=tiger%20animal%20'
 images = []
-max_pages = 10
+max_pages = 40
+for page in range(0, max_pages + 1):
+    cur_URL = URL + str(page)
+    html_page = requests.get(cur_URL, headers).text
+    soup = BeautifulSoup(html_page, 'lxml')
+    print(soup)
+    break
+    divs = soup.find_all('div', {"class": re.compile(r"serp-item serp-item_type_search serp-item_group_search serp-item_pos_\d+ serp-item_scale_yes justifier__item i-bem$")})
+    for i in range(len(divs)):
+        img = re.findall(r'"img_href":"([^"]+)"', str(divs[i]))
+        images += img
+print(len(images))
+for i in range(len(images)):
+    print(images[i])
+    download_picture('tiger', i, images[i])
+del images
+images = []
+URL = 'https://yandex.ru/images/search?text=leopard%20animal%20'
 for page in range(1, max_pages + 1):
     cur_URL = URL + str(page)
     html_page = requests.get(cur_URL, headers).text
     soup = BeautifulSoup(html_page, 'lxml')
+    img = re.findall(r'"origin":\{"w":\w+,"h":\w+,"url":"([^"]+)"\}', str(divs[i]))
     divs = soup.find_all('div', {"class": re.compile(r"serp-item serp-item_type_search serp-item_group_search serp-item_pos_\d+ serp-item_scale_yes justifier__item i-bem$")})
     for i in range(len(divs)):
         img = re.findall(r'"origin":\{"w":\w+,"h":\w+,"url":"([^"]+)"\}', str(divs[i]))
-        images += img
-print(len(images))
-# html_page = requests.get(URL+'search?text=' + 'tiger', headers={"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/104.0.5112.102 Safari/537.36 OPR/90.0.4480.84 (Edition Yx 05)"})
-# soup = BeautifulSoup(html_page.text, 'lxml')
-# a = soup.find_all('div', class_="serp-item serp-item_type_search serp-item_group_search ")
-# print(soup)
-# print('\n', a)
-# os.mkdir('dataset')
-# os.mkdir('dataset/tiger')
-# os.mkdir('dataset/leopard')
-# cv2.imwrite('python/dataset/tiger', img[0])
-# за каждую итерацию загружается > 100 фотографий
-'''
-filename = 'dataset/tiger/0000.jpg'
-image = requests.get('https://www.fonstola.ru/images/201509/fonstola.ru_202497.jpg')
-print(image)
-directory = open(filename, 'wb')
-directory.write(image.content)
-directory.close()
-
-img = []
-max_pages = 9
-for i in range(max_pages):
-    a = re.findall(r'"origin":\{"w":\w+,"h":\w+,"url":"([^"]+)"\}', html_page.text)
-    img = img + a
-    next_page = re.findall(r'href="/images/([^"]+)"[^E]+Ещё картинки', html_page.text)
-    # print(next_page)
-    if len(next_page) == 0:
-        break
-    html_page = requests.get(URL + next_page[0])
-print(len(img))'''
+        images.append(img[0])
+for i in range(len(images)):
+    download_picture('leopard', i, images[i])
+del images
