9d9ebd4491c1c23527d1c956b6e6801ca8be7006
pavel <tomashaytis@gmail.com>
2022-09-27 21:37:07 +0400

    Удалось скачать фотографии без ошибок в большом количестве, продублировал этот код для картинок второго класса, удалил лишние комментарии, добавил условие if __name__ == '__main__':

--- Lab1/Lab1.py
+++ Lab1/Lab1.py
@@ -5,7 +5,7 @@ from bs4 import BeautifulSoup
 
 def download_picture(direct, img_id, img_address):
     filename = f'dataset/{direct}/{str(img_id).rjust(4, "0")}.jpg'
-    picture = requests.get(f'https{img_address}', headers)
+    picture = requests.get(f'https:{img_address}', headers)
     if picture.status_code:
         direct = open(filename, 'wb')
         direct.write(picture.content)
@@ -24,8 +24,8 @@ def directory_manager(direct):
 directory_manager('dataset/tiger')
 directory_manager('dataset/leopard')
 headers = {"User-Agent": "Mozilla/5.0"}
-name = 'tiger'
 URL = 'https://yandex.ru/images/'
+name = 'tiger'
 images = []
 page = 0
 while len(images) <= 1000:
@@ -33,43 +33,29 @@ while len(images) <= 1000:
     soup = BeautifulSoup(html_page, 'lxml')
     page_img = soup.find_all('img')
     for image in page_img:
-        s = str(image['src'])
-        if len(s) and s[0] == '/':
+        if len(image['src']) and image['src'][0] == '/':
             images.append(image['src'])
             print(len(images))
     page += 1
-print(images)
-print(len(images))
 for i in range(len(images)):
-    print(images[i])
     download_picture(name, i, images[i])
 del images
-'''
-max_pages = 40
-for page in range(0, max_pages + 1):
-    cur_URL = URL + str(page)
-    html_page = requests.get(cur_URL, headers).text
-    soup = BeautifulSoup(html_page, 'lxml')
-    divs = soup.find_all('div', {"class": re.compile(r"serp-item serp-item_type_search serp-item_group_search serp-item_pos_\d+ serp-item_scale_yes justifier__item i-bem$")})
-    for i in range(len(divs)):
-        img = re.findall(r'"img_href":"([^"]+)"', str(divs[i]))
-        images += img
-print(len(images))
-for i in range(len(images)):
-    print(images[i])
-    download_picture(tiger, i, images[i])
-del images
+name = 'leopard'
 images = []
-URL = 'https://yandex.ru/images/search?text=leopard%20animal%20'
-for page in range(1, max_pages + 1):
-    cur_URL = URL + str(page)
-    html_page = requests.get(cur_URL, headers).text
+page = 0
+while len(images) <= 1000:
+    html_page = requests.get(f'{URL}search?p={page}&text={name}&lr=51&rpt=image', headers).text
     soup = BeautifulSoup(html_page, 'lxml')
-    img = re.findall(r'"origin":\{"w":\w+,"h":\w+,"url":"([^"]+)"\}', str(divs[i]))
-    divs = soup.find_all('div', {"class": re.compile(r"serp-item serp-item_type_search serp-item_group_search serp-item_pos_\d+ serp-item_scale_yes justifier__item i-bem$")})
-    for i in range(len(divs)):
-        img = re.findall(r'"origin":\{"w":\w+,"h":\w+,"url":"([^"]+)"\}', str(divs[i]))
-        images.append(img[0])
+    page_img = soup.find_all('img')
+    for image in page_img:
+        if len(image['src']) and image['src'][0] == '/':
+            images.append(image['src'])
+            print(len(images))
+    page += 1
 for i in range(len(images)):
-    download_picture(leopard, i, images[i])
-del images'''
+    download_picture(name, i, images[i])
+del images
+
+if __name__ == '__main__':
+    directory_manager()
+    download_picture()
