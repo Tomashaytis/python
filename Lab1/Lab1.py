385d6389b2dda062e6fb8938fafa6265664decc4
pavel <tomashaytis@gmail.com>
2022-09-29 11:53:46 +0400

    Удалил лишние модули, создал файл requirements.txt с версиями установленных модулей, добавил описание к функциям

--- Lab1/Lab1.py
+++ Lab1/Lab1.py
@@ -4,15 +4,33 @@ from bs4 import BeautifulSoup
 
 
 def download_picture(direct, img_id, img_address):
+    """
+    Качает картинку и сохраняет её в выбранную директорию.
+    Имя скаченной картинки формируется на основе её идентификатора.
+
+    :param direct: путь к директории.
+    :param img_id: идентификатор картинки.
+    :param img_address: сетевой адрес картинки.
+    :return: успешно или неуспешно прошла операция загрузки.
+    """
     filename = f'dataset/{direct}/{str(img_id).rjust(4, "0")}.jpg'
     picture = requests.get(f'https:{img_address}', headers)
     if picture.status_code:
         direct = open(filename, 'wb')
         direct.write(picture.content)
         direct.close()
+        return True
+    return False
 
 
 def directory_manager(direct):
+    """
+    Создаёт новую директорию с выбранным именем, а также все промежуточные директории, если их нет.
+    Если директория уже создана, пересоздаёт её.
+
+    :param direct: имя новой директории (может содержать и промежуточные директории).
+    :return: нет возвращаемого значения.
+    """
     while True:
         try:
             os.makedirs(direct)
@@ -53,7 +71,8 @@ while len(images) <= 1000:
             print(len(images))
     page += 1
 for i in range(len(images)):
-    download_picture(name, i, images[i])
+    if not download_picture(name, i, images[i]):
+        print(f'Ошибка загрузки изображения. Его id: {i}')
 del images
 
 if __name__ == '__main__':
