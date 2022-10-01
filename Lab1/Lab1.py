adbc0a4effa4441b7f7c5998a0ea1abef0679285
pavel <tomashaytis@gmail.com>
2022-10-01 16:13:39 +0400

    Начал выводить системные сообщения об ошибках, получил возможность увидеть сообщения уровня INFO, избавился от переменной checker, добавил новый параметр min_page_photos у функции download_quantity_of_photos

--- Lab1/Lab1.py
+++ Lab1/Lab1.py
@@ -1,9 +1,12 @@
 import os
 import requests
 from bs4 import BeautifulSoup
-from logging import warning
+import logging
 from tqdm import tqdm
 HEADERS = {"User-Agent": "Mozilla/5.0"}
+
+logger = logging.getLogger()
+logger.setLevel('INFO')
 
 
 def download_picture(filename, img_address):
@@ -20,8 +23,8 @@ def download_picture(filename, img_address):
         direct = open(filename, 'wb')
         direct.write(picture.content)
         direct.close()
-    except OSError:
-        warning(f' Загрузка изображения не удалась.')
+    except OSError as err:
+        logging.warning(f' При попытке загрузки изображения произошла ошибка:\n{err}')
 
 
 def directory_manager(directory):
@@ -35,12 +38,12 @@ def directory_manager(directory):
     folder = os.path.split(directory)
     try:
         os.makedirs(directory)
-        print(f'Папка {folder[1]} успешно создана.')
-    except OSError:
-        print(f'Папка {folder[1]} уже существует.')
+        logging.info(f' Папка {folder[1]} успешно создана.')
+    except OSError as err:
+        logging.info(f' При попытке создания папки {folder[1]} произошла ошибка:\n{err}.')
 
 
-def download_quantity_of_photos(search_name, quantity, directory, url='https://yandex.ru/images/'):
+def download_quantity_of_photos(search_name, quantity, directory, url='https://yandex.ru/images/', min_page_photos=5):
     """
     Получает html код страницы https://yandex.ru/images для выбранного поискового запроса.
     Находит в этом коде выбранное количество картинок, соответствующих запросу.
@@ -50,25 +53,27 @@ def download_quantity_of_photos(search_name, quantity, directory, url='https://y
     :param quantity: количество соответствующих запросу картинок, подлежащих скачиванию.
     :param directory: путь к директории, куда будут скачиваться картинки.
     :param url: адрес страницы, с которой будет осуществляться поиск.
+    :param min_page_photos: Минимальное количество картинок на странице сайта.
     :return: Нет возвращаемого значения.
     """
-    checker = 0
-    images = []
-    page = 0
     no_repeat = 0
     repeaters = 0
+    page = 0
+    images = []
     bar = tqdm(total=quantity, desc='Процесс поиска изображений: ', ncols=120)
     while len(images) <= quantity:
         html_page = requests.get(f'{url}search?p={page}&text={search_name}&lr=51&rpt=image', HEADERS)
         if html_page.status_code != 200:
-            warning(f' Запрос не был выполнен. Код ответа сайта: {html_page.status_code}')
-            return
-        if checker > 10:
-            warning(' На страницах результатов нет картинок.')
+            del bar
+            logging.warning(f' Запрос не был выполнен. Код ответа сайта: {html_page.status_code}')
             return
         html_page = html_page.text
         soup = BeautifulSoup(html_page, 'lxml')
         page_img = soup.find_all('img')
+        if len(page_img) < min_page_photos:
+            del bar
+            logging.warning(' На страницах результатов нет картинок.')
+            return
         for image in page_img:
             if len(image['src']) and image['src'][0] == '/':
                 if no_repeat == 30:
@@ -80,8 +85,6 @@ def download_quantity_of_photos(search_name, quantity, directory, url='https://y
                     bar.update(1)
                 else:
                     repeaters -= 1
-                checker = 0
-        checker += 1
         page += 1
     del bar
     for i in tqdm(range(len(images)), desc='Процесс скачивания изображений: ', ncols=120):
@@ -95,5 +98,5 @@ if __name__ == '__main__':
     leopard_path = os.path.join('dataset', 'leopard')
     directory_manager(tiger_path)
     directory_manager(leopard_path)
-    download_quantity_of_photos('tiger', 1100, tiger_path)
-    download_quantity_of_photos('leopard', 1100, leopard_path)
+    download_quantity_of_photos('tiger', 1000, tiger_path)
+    download_quantity_of_photos('leopard', 1000, leopard_path)
