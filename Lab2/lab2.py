7476126f0908a7fe3b3aadc4c9c1dfa449151e31
pavel <tomashaytis@gmail.com>
2022-10-16 16:46:23 +0400

    Добавил в класс обработку исключений, использовал модуль logging, добавил проверку на количество файлов в папке с изображениями. Изменил содержание файла ".gitignore".

--- Lab2/lab2.py
+++ Lab2/lab2.py
@@ -1,47 +1,60 @@
 import csv
 import os
+import logging
 from typing import List
 
+logger = logging.getLogger()
+logger.setLevel('INFO')
 
-# классы
-# обработка исключений
-# типы
-# class Create: - создаёт аннотацию, итераторы
+
 class ClassManager:
     _dir: str
 
-    def __init__(self, cur_dir=os.getcwd()) -> None:
+    @property
+    def dir(self) -> str:
+        return self._dir
+
+    @dir.setter
+    def dir(self, directory: str) -> None:
+        self._dir = directory
+
+    def __init__(self, cur_dir: str = os.getcwd()) -> None:
         self._dir = cur_dir
 
     def create_annotation(self) -> None:
-        # Исключение
-        file = open(os.path.join(self._dir, 'annotation.csv'), 'w', newline='')
-        columns = ['absolute path', 'relative path', 'class']
-        class_folders = []
-        for root, dirs, files in os.walk(self._dir):
-            for class_name in dirs:
-                class_folder = {'absolute path': os.path.abspath(class_name),
-                                'relative path': os.path.join(self._dir, class_name),
-                                'class': class_name}
-                class_folders.append(class_folder)
-        writer = csv.DictWriter(file, fieldnames=columns)
-        writer.writeheader()
-        writer.writerows(class_folders)
-        file.close()
+        try:
+            with open(os.path.join(self._dir, 'annotation.csv'), 'w', newline='') as file:
+                columns = ['absolute path', 'relative path', 'class']
+                class_folders = []
+                for root, dirs, files in os.walk(self._dir):
+                    for class_name in dirs:
+                        cur_dir = os.path.join(root, class_name)
+                        if len(os.listdir(cur_dir)) >= 1000:
+                            class_folder = {'absolute path': os.path.abspath(cur_dir),
+                                            'relative path': os.path.relpath(cur_dir),
+                                            'class': class_name}
+                            class_folders.append(class_folder)
+                writer = csv.DictWriter(file, fieldnames=columns)
+                writer.writeheader()
+                writer.writerows(class_folders)
+                pass
+        except OSError as err:
+            logging.warning(f' При попытке создания аннотации по пути {self._dir} произошла ошибка:\n{err}.')
 
     def read_annotation(self) -> List:
-        filename = os.path.join(self._dir, 'annotation.csv')
-        file = open(filename, 'r', newline='')
-        rows = csv.DictReader(file)
         res = []
-        for row in rows:
-            res.append([row['absolute path'], row['relative path'], row['class']])
-        file.close()
+        try:
+            with open(os.path.join(self._dir, 'annotation.csv'), 'r', newline='') as file:
+                rows = csv.DictReader(file)
+                for row in rows:
+                    res.append([row['absolute path'], row['relative path'], row['class']])
+        except OSError as err:
+            logging.warning(f' При попытке открытия аннотации по пути {self._dir} произошла ошибка:\n{err}.')
         return res
 
 
 if __name__ == "__main__":
-    cm = ClassManager('lab1')
+    cm = ClassManager('D:\\Projects\\Python\\python')
     cm.create_annotation()
     a = cm.read_annotation()
     for i in a:
