150bc046cf04c7ff9b9176098b36f1ec6bd217c1
pavel <tomashaytis@gmail.com>
2022-11-02 10:14:13 +0400

    Изменил положение виджетов в сетке, добавил возможность просмотра изображения

--- Lab3/main_window.py
+++ Lab3/main_window.py
@@ -1,33 +1,41 @@
+import os
 import sys
 from PyQt6.QtCore import QSize
 from PyQt6.QtWidgets import QApplication, QMainWindow, QPushButton, QFileDialog, QLabel, QGridLayout, QWidget
 from PyQt6.QtGui import QPixmap
+from annotation import CLASSES, Annotation
 
 
 class MainWindow(QMainWindow):
     def __init__(self):
         super(MainWindow, self).__init__()
-        self.setWindowTitle("My App")
+        self.setWindowTitle("lab-3")
         dataset_path = QFileDialog.getExistingDirectory(self, 'Введите путь к папке исходного датасета')
-        src = QLabel(f'Текущий исходный датасет: {dataset_path}', self)
-        src.setFixedSize(QSize(400, 20))
+        src = QLabel(f'Исходный датасет:\n{dataset_path}', self)
         layout = QGridLayout()
-        layout.addWidget(src, 0, 1)
+        layout.addWidget(src, 0, 0)
+        create_annotation_button = QPushButton("Сформировать аннотацию")
+        create_annotation_button.setFixedSize(QSize(250, 50))
+        layout.addWidget(create_annotation_button, 1, 0)
         copy_dataset_button = QPushButton("Скопировать датасет")
-        copy_dataset_button.setFixedSize(QSize(200, 50))
-        layout.addWidget(copy_dataset_button, 3, 5)
+        copy_dataset_button.setFixedSize(QSize(250, 50))
+        layout.addWidget(copy_dataset_button, 2, 0)
         copy_random_dataset_button = QPushButton("Рандомизировать датасет")
-        copy_random_dataset_button.setFixedSize(QSize(200, 50))
-        layout.addWidget(copy_random_dataset_button, 4, 5)
+        copy_random_dataset_button.setFixedSize(QSize(250, 50))
+        layout.addWidget(copy_random_dataset_button, 3, 0)
         next_tiger_button = QPushButton("Следующий тигр")
-        next_tiger_button.setFixedSize(QSize(200, 50))
-        layout.addWidget(next_tiger_button, 5, 0)
+        next_tiger_button.setFixedSize(QSize(250, 50))
+        layout.addWidget(next_tiger_button, 4, 0)
         next_leopard_button = QPushButton("Следующий леопард")
-        next_leopard_button.setFixedSize(QSize(200, 50))
-        layout.addWidget(next_leopard_button, 5, 2)
+        next_leopard_button.setFixedSize(QSize(250, 50))
+        layout.addWidget(next_leopard_button, 5, 0)
+        an = Annotation(os.path.join(dataset_path, CLASSES[0]))
+        cur_tiger = an.first_instance()[1]
+        an = Annotation(os.path.join(dataset_path, CLASSES[1]))
+        cur_leopard = an.first_instance()[1]
         label_image = QLabel('Здесь будет картинка')
-        label_image.setFixedSize(QSize(500, 400))
-        layout.addWidget(label_image, 1, 0, 4, 4)
+        label_image.setPixmap(QPixmap(cur_tiger))
+        layout.addWidget(label_image, 1, 1, 6, 2)
         central_widget = QWidget()
         central_widget.setLayout(layout)
         self.setCentralWidget(central_widget)
