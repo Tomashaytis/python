144990b6c43fbe62d5787d1ee830e6902bbbbad6
pavel <tomashaytis@gmail.com>
2022-11-05 13:19:57 +0400

    Реализовал создание аннотации, копирование и рандомизирование датасета (обработчики событий). Перенёс константу CLASSES в основной файл. Изменил логику работы функции random_dataset(). Она теперь только переименовывает файлы (Это необходимо для рандомизации исходного датасета). Массив СЧ теперь перемешивается функцией shuffle(). Добавил блокировку кнопок на случай неправильного исходного датасета.

--- Lab3/dataset_random.py
+++ Lab3/dataset_random.py
@@ -1,38 +1,39 @@
 import os
 import logging
-from random import randint
-from annotation import Annotation
+from random import randint, shuffle
+from annotation import Annotation, CLASSES
 from dataset_replication import copy_dataset
 
 
-def random_dataset(dataset: str, path_to_copy: str) -> None:
+def random_dataset(dataset_path: str) -> None:
     """
-    Копирует экземпляры класса из папки dataset по пути path_to_copy.
+    Переименовывает экземпляры класса в папке по пути dataset_path.
     Каждый экземпляр получит случайный номер от 0000 до 9999.
 
-    :param dataset: Путь к датасету с экземплярами класса.
-    :param path_to_copy: Путь до папки в которую нужно скопировать датасет.
+    :param dataset_path: Путь до папки в которой будет происходить рандомизация датасета.
     :return: Нет возвращаемого значения.
     """
-    copy_dataset(dataset, path_to_copy)
-    copy_an = Annotation(path_to_copy)
-    instances = copy_an.read()
+    an = Annotation(dataset_path)
+    instances = an.read()
     random_numbers = set()
     while len(random_numbers) != len(instances):
         random_numbers.add(randint(0, 9999))
     random_numbers = list(random_numbers)
+    shuffle(random_numbers)
     for inst, rand in zip(instances, random_numbers):
         new_filename = f'{rand:04d}.jpg'
         new_filename_path = os.path.join(os.path.split(inst[1])[0], new_filename)
         old_filename_path = inst[1]
         try:
             os.rename(old_filename_path, new_filename_path)
-            copy_an.add(inst[2], new_filename)
+            an.add(inst[2], new_filename)
         except OSError as err:
             logging.warning(f' При попытке переименования файла по пути {old_filename_path} в папке '
-                            f'{path_to_copy} произошла ошибка:\n{err}.')
-    copy_an.create()
+                            f'{dataset_path} произошла ошибка:\n{err}.')
+    an.create()
 
 
 if __name__ == "__main__":
-    random_dataset('dataset1', 'who')
+    for i in CLASSES:
+        copy_dataset(os.path.join('dataset', i), 'who')
+    random_dataset('who')
--- Lab3/main_window.py
+++ Lab3/main_window.py
@@ -3,16 +3,20 @@ import sys
 from PyQt6.QtCore import QSize, Qt
 from PyQt6.QtWidgets import QApplication, QMainWindow, QPushButton, QFileDialog, QLabel, QGridLayout, QWidget
 from PyQt6.QtGui import QPixmap
-from annotation import CLASSES, Annotation
+from annotation import Annotation
 from iterator import InstanceIterator
+from dataset_replication import copy_dataset
+from dataset_random import random_dataset
+
+CLASSES = ["tiger", "leopard"]
 
 
 class MainWindow(QMainWindow):
     def __init__(self):
         super(MainWindow, self).__init__()
         self.setWindowTitle("lab-3")
-        dataset_path = QFileDialog.getExistingDirectory(self, 'Введите путь к папке исходного датасета')
-        src = QLabel(f'Исходный датасет:\n{dataset_path}', self)
+        self.dataset_path = QFileDialog.getExistingDirectory(self, 'Введите путь к папке исходного датасета')
+        src = QLabel(f'Исходный датасет:\n{self.dataset_path}', self)
         layout = QGridLayout()
         layout.addWidget(src, 0, 0)
         create_annotation_button = QPushButton("Сформировать аннотацию")
@@ -35,26 +39,46 @@ class MainWindow(QMainWindow):
         self.next_leopard_button.setFixedSize(QSize(250, 50))
         self.next_leopard_button.clicked.connect(self.next_leopard)
         layout.addWidget(self.next_leopard_button, 5, 0)
-        an = Annotation(os.path.join(dataset_path, CLASSES[0]))
-        self.cur_tiger = InstanceIterator(an.first_instance()[1])
-        an = Annotation(os.path.join(dataset_path, CLASSES[1]))
-        self.cur_leopard = InstanceIterator(an.first_instance()[1])
         self.label_image = QLabel('Здесь будет картинка')
         self.label_image.setMinimumSize(QSize(500, 300))
         self.label_image.setAlignment(Qt.AlignmentFlag.AlignCenter)
         layout.addWidget(self.label_image, 1, 1, 6, 2)
+        if not (os.path.exists(os.path.join(self.dataset_path, CLASSES[0])) or
+                os.path.exists(os.path.join(self.dataset_path, CLASSES[1]))):
+            create_annotation_button.setEnabled(False)
+            copy_dataset_button.setEnabled(False)
+            copy_random_dataset_button.setEnabled(False)
+            self.next_tiger_button.setEnabled(False)
+            self.next_leopard_button.setEnabled(False)
+            self.label_image.setText('Ошибка!\n'
+                                     'В исходном датасете отсутствует одна из папок классов: "tiger" или "leopard"')
+        else:
+            an = Annotation(os.path.join(self.dataset_path, CLASSES[0]))
+            self.cur_tiger = InstanceIterator(an.first_instance()[1])
+            an = Annotation(os.path.join(self.dataset_path, CLASSES[1]))
+            self.cur_leopard = InstanceIterator(an.first_instance()[1])
         central_widget = QWidget()
         central_widget.setLayout(layout)
         self.setCentralWidget(central_widget)
         self.show()
 
     def create_annotation(self):
-        print("Абра-кадабра")
+        when_dir = QFileDialog.getExistingDirectory(self, 'Введите путь к папке, в которой будет создана аннотация')
+        for cls in CLASSES:
+            an = Annotation(os.path.join(self.dataset_path, cls), when_dir)
+            an.add_from_old_annotation()
+            an.create_for_class(cls)
+
     def dataset_copy(self):
-        print("Абра-кадабра")
+        when_dir = QFileDialog.getExistingDirectory(self, 'Введите путь к папке, в которую будет скопирован датасет')
+        for cls in CLASSES:
+            copy_dataset(os.path.join(self.dataset_path, cls), when_dir)
 
     def dataset_random(self):
-        print("Абра-кадабра")
+        when_dir = QFileDialog.getExistingDirectory(self, 'Введите путь к папке, в которую будет скопирован датасет')
+        for cls in CLASSES:
+            copy_dataset(os.path.join(self.dataset_path, cls), when_dir)
+        random_dataset(when_dir)
 
     def next_tiger(self):
         self.label_image.setPixmap(QPixmap(self.cur_tiger.__iter__()))
@@ -71,7 +95,6 @@ class MainWindow(QMainWindow):
         except StopIteration:
             self.label_image.setText("Леопарды закончились")
             self.next_leopard_button.setEnabled(False)
-
 
 
 if __name__ == '__main__':
