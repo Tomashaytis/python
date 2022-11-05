08567dc661251a2579b8ca1fc5c744cd35174797
pavel <tomashaytis@gmail.com>
2022-11-05 13:56:55 +0400

    Добавил обработку исключений в некоторых местах. Добавил файл requirements.txt. Вынес в отдельную функцию инициализацию интерфейса. Добавил описания к функциям класса MainWindow.

--- Lab3/main_window.py
+++ Lab3/main_window.py
@@ -1,3 +1,4 @@
+import logging
 import os
 import sys
 from PyQt6.QtCore import QSize, Qt
@@ -12,8 +13,30 @@ CLASSES = ["tiger", "leopard"]
 
 
 class MainWindow(QMainWindow):
+    """
+    Класс реализует графический интерфейс для работы с функциями из 2 лабораторной.
+    """
+    dataset_path: str
+    next_tiger_button: QPushButton
+    next_leopard_button: QPushButton
+    label_image: QLabel
+    cur_tiger: InstanceIterator
+    cur_leopard: InstanceIterator
+
     def __init__(self):
+        """
+        Инициализация класса MainWindow.
+        """
         super(MainWindow, self).__init__()
+        self.init_ui()
+
+    def init_ui(self) -> None:
+        """
+        Функция инициализирует графический интерфейс приложения.
+        От пользователя запрашивается путь к исходному датасету.
+
+        :return: Нет возвращаемого значения.
+        """
         self.setWindowTitle("lab-3")
         self.dataset_path = QFileDialog.getExistingDirectory(self, 'Введите путь к папке исходного датасета')
         src = QLabel(f'Исходный датасет:\n{self.dataset_path}', self)
@@ -62,39 +85,76 @@ class MainWindow(QMainWindow):
         self.setCentralWidget(central_widget)
         self.show()
 
-    def create_annotation(self):
+    def create_annotation(self) -> None:
+        """
+        Функция создаёт аннотацию исходного датасета, реагируя на нажатие кнопки "Сформировать аннотацию".
+        Пользователь должен выбрать путь к папке-назначения для файла-аннотации.
+
+        :return: Нет возвращаемого значения.
+        """
         when_dir = QFileDialog.getExistingDirectory(self, 'Введите путь к папке, в которой будет создана аннотация')
         for cls in CLASSES:
             an = Annotation(os.path.join(self.dataset_path, cls), when_dir)
             an.add_from_old_annotation()
             an.create_for_class(cls)
 
-    def dataset_copy(self):
+    def dataset_copy(self) -> None:
+        """
+        Функция копирует экземпляры исходного датасета, реагируя на нажатие кнопки "Скопировать датасет".
+        Пользователь должен выбрать путь к папке-назначения, в которую будет скопирован датасет.
+
+        :return: Нет возвращаемого значения.
+        """
         when_dir = QFileDialog.getExistingDirectory(self, 'Введите путь к папке, в которую будет скопирован датасет')
         for cls in CLASSES:
             copy_dataset(os.path.join(self.dataset_path, cls), when_dir)
 
-    def dataset_random(self):
+    def dataset_random(self) -> None:
+        """
+        Функция копирует экземпляры исходного датасета, реагируя на нажатие кнопки "Рандомизировать датасет".
+        Пользователь должен выбрать путь к папке-назначения, в которую будет скопирован датасет.
+        Каждый экземпляр будет иметь случайный номер от 0000 до 9999.
+
+        :return: Нет возвращаемого значения.
+        """
         when_dir = QFileDialog.getExistingDirectory(self, 'Введите путь к папке, в которую будет скопирован датасет')
         for cls in CLASSES:
             copy_dataset(os.path.join(self.dataset_path, cls), when_dir)
         random_dataset(when_dir)
 
-    def next_tiger(self):
+    def next_tiger(self) -> None:
+        """
+        Функция меняет текущее содержание метки Label на изображение экземпляра с классом тигр, реагируя на нажатие
+        кнопки "Следующий тигр".
+        Если изображения закончатся - оповестит об этом.
+
+        :return: Нет возвращаемого значения.
+        """
         self.label_image.setPixmap(QPixmap(self.cur_tiger.__iter__()))
         try:
             self.cur_tiger.__next__()
         except StopIteration:
             self.label_image.setText("Тигры закончились")
             self.next_tiger_button.setEnabled(False)
+        except OSError as err:
+            logging.warning(f'При работе итератора была вызвана ошибка:\n{err}')
 
-    def next_leopard(self):
+    def next_leopard(self) -> None:
+        """
+        Функция меняет текущее содержание метки Label на изображение экземпляра с классом leopard, реагируя на нажатие
+        кнопки "Следующий леопард".
+        Если изображения закончатся - оповестит об этом.
+
+        :return: Нет возвращаемого значения.
+        """
         self.label_image.setPixmap(QPixmap(self.cur_leopard.__iter__()))
         try:
             self.cur_leopard.__next__()
         except StopIteration:
             self.label_image.setText("Леопарды закончились")
             self.next_leopard_button.setEnabled(False)
+        except OSError as err:
+            logging.warning(f'При работе итератора была вызвана ошибка:\n{err}')
 
 
 if __name__ == '__main__':
