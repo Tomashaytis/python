e432c316912f0e7a61d7be6dc6b09cf99353f99e
pavel <tomashaytis@gmail.com>
2022-11-20 20:56:34 +0400

    Реализовал функцию grouper, группирующую датафрейм с вычислением максимального, минимального и среднего значения по количеству пикселей. Добавил к ней docstring.

--- Lab4/main.py
+++ Lab4/main.py
@@ -27,7 +27,7 @@ def class_mark_filter(df: pd.DataFrame, class_mark: str) -> pd.DataFrame:
     """
     Функция возвращает отфильтрованный по метке класса датафрейм.
 
-    :param df: датафрейм, который требуется отфильтровать.
+    :param df: Датафрейм, который требуется отфильтровать.
     :param class_mark: Метка класса, по которой производится фильтрация.
     :return: Отфильтрованный датафрейм.
     """
@@ -40,13 +40,25 @@ def multi_filter(df: pd.DataFrame, max_width: int, max_height: int, class_mark: 
     Результатом будет датафрейм, состоящий из изображений, удовлетворяющих условиям:
     width <= max_width, height <= max_height, и метка класса соответствует заданной.
 
-    :param df: датафрейм, который требуется отфильтровать.
+    :param df: Датафрейм, который требуется отфильтровать.
     :param max_width: Максимальная ширина изображения.
     :param max_height: Максимальная высота изображения.
     :param class_mark: Метка класса, по которой производится фильтрация.
     :return: Отфильтрованный датафрейм.
     """
     return df[((df.class_mark == class_mark) & (df.width <= max_width) & (df.height <= max_height))]
+
+
+def grouper(df: pd.DataFrame) -> tuple:
+    """
+    Функция выполняет группировки датафрейма по метке класса, дополнительно вычисляя максимальное, минимальное
+    и среднее значение по количеству пикселей. Возвращает кортеж из 3 получившихся группировок.
+
+    :param df: Датафрейм, по которому строятся группировки.
+    :return: Кортеж из 3 группировок: максимальное, минимальное и среднее количество пикселей изображения.
+    """
+    df['pixels'] = df['width'] * df['height'] * df['channels']
+    return df.groupby('class_mark').max(), df.groupby('class_mark').min(), df.groupby('class_mark').mean()
 
 
 if __name__ == "__main__":
@@ -56,5 +68,5 @@ if __name__ == "__main__":
     mask = (instance_df.class_mark == CLASSES[1])
     instance_df['numerical_class_mark'] = mask.astype(int)
     instance_df['width'], instance_df['height'], instance_df['channels'] = image_shapes(instance_df['absolute_path'])
-    filter_df = multi_filter(instance_df, 480, 319, CLASSES[1])
-    print(filter_df['class_mark'])
+    group_df_max, group_df_min, group_df_mean = grouper(instance_df)
+    print(group_df_mean)
